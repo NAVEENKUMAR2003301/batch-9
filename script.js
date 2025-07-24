71ec3ddbbf856a2f57ffaee9b253b54a1611dfbe
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2025-07-24 12:38:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,133 @@
+// variables 
+
+// 1.var 
+// 2.let 
+// 3.const
+
+// var 
+
+// var a = 100;
+
+// var a;
+
+// console.log(a);
+// a = "run";
+// console.log(a);
+
+// let 
+
+// let b = 200;
+
+// b = "eight"
+
+// let b = 200
+
+
+// console.log(b);
+
+// printing statement 
+
+// const 
+
+// const c = 200
+
+// const c=300
+
+// console.log(c);
+
+// const a = prompt("what is your name");
+
+// console.log(a);
+
+// alert("hello everyone listen carefully")
+
+// confirm("hello everyone is it ok")
+
+
+// document.writeln("hello everyone")
+
+
+
+// console method 
+
+// let newAge = 25
+
+// console.log(newAge);
+// console.warn(newAge);
+// console.error(newAge);
+// console.clear()
+
+
+// dataTypes 
+
+// 1.primitive DataType 
+// 2.non primitive DataType
+
+// 1. primitive DataType 
+
+// 1.Number 
+// 2.string 
+// 3.boolean 
+// 4.null 
+// 5.undefined 
+
+// 2. non Primitive DataType 
+
+// 1.array 
+// 2.object 
+// 3.function 
+// 4.date 
+
+
+// 1.Explain 
+
+// 1.number 
+
+console.log(typeof(100));
+
+// 2.string 
+
+console.log(typeof("abc"));
+
+// 3.boolean 
+
+console.log(typeof(true));
+
+// 4.null 
+
+// undefined 
+console.log(a);
+var a = 200
+
+
+// non primitive dataType 
+
+// 1.array 
+
+let fruit = ["apple","orange","banana","guava","cherry"]
+
+console.log(fruit);
+console.log(fruit[2]);
+console.log(fruit[fruit.length-1]);
+
+// 2.object 
+
+let employee = {
+    eName : "ragul",
+    eRole : "frontend Developer"
+}
+
+console.log(employee);
+
+console.log(employee.eRole);
+
+
+
+
+
+
+
+
+
+
+
